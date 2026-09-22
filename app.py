37cd880a62e985b4dc2634cfca678ccf3179fc3c
adithyas843-sketch <adithyas843@gmail.com>
2026-09-22 17:35:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 
 import streamlit as st
 import pandas as pd
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parent))
-
-from modules.po_extractor import extract_po_data
-from modules.excel_exporter import create_excel_report
+from po_extractor import extract_po_data
+from excel_exporter import create_excel_report
 st.set_page_config(page_title="PO Extractor", layout="wide")
 
 st.title("Purchase Order Extractor")
